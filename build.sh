f7662cf71ad1cbed37ee0c9857873d2913c2aa54
Darwin Gautalius <darwin.gautalius@gdplabs.id>
2019-09-11 10:07:19 +0700

    update resources

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build . --tag nest:latest
+docker tag nest:latest 177188985094.dkr.ecr.us-east-1.amazonaws.com/example/nest:latest
+docker push 177188985094.dkr.ecr.us-east-1.amazonaws.com/example/nest:latest
---
+++ deploy/resources-ecs-execution_role.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_role_policy_document" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_exec" {
+  name   = "ecsExecutionRolePolicy"
+  role = "${aws_iam_role.ecs_task_execution_role.id}"
+  policy = "${data.aws_iam_policy_document.ecs_task_execution_role_policy_document.json}"
+}
